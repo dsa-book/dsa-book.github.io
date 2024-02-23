a0854a62eaeb1d8890671f4df7efa1ebb3559d2c
interactive-dsa-book <kumar6017drive@gmail.com>
2024-02-23 15:53:42 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-# Get the path to the Python binary folder
-PYTHON_BIN_PATH=$(dirname "$(which python3)")
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --colab)
+            COLAB="$2"
+            shift
+            shift
+            ;;
+        --platform)
+            PLATFORM="$2"
+            shift
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+# Function to export path based on platform
+export_path() {
+    if [ "$PLATFORM" = "mac" ]; then
+        # Get the path to the Python binary folder on Mac
+        PYTHON_BIN_PATH=$(dirname "$(which python3)")
+        export PATH="$PATH:$PYTHON_BIN_PATH"
+    elif [ "$PLATFORM" = "windows" ]; then
+        # Get the path to the Python binary folder on Windows
+        PYTHON_BIN_PATH=$(where python | head -n 1)
+        PYTHON_BIN_PATH=$(dirname "$PYTHON_BIN_PATH")
+        setx PATH "%PATH%;%PYTHON_BIN_PATH%"
+    fi
+}
 
 # Export the path only if COLAB is not set or set to false
 if [ -z "$COLAB" ] || [ "$COLAB" = false ]; then
-    export PATH="$PATH:$PYTHON_BIN_PATH"
+    export_path
 fi
 
 # Check if local-req.txt exists
