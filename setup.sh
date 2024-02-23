ebe714830476d16de4496d507ef47ff1b26ba478
interactive-dsa-book <101341950+interactive-dsa-book@users.noreply.github.com>
2024-02-23 18:36:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
-
-# Parse command-line arguments
-while [[ $# -gt 0 ]]; do
-    key="$1"
-    case $key in
-        --colab)
-            COLAB="$2"
-            shift
-            shift
-            ;;
-        --platform)
-            PLATFORM="$2"
-            shift
-            shift
-            ;;
-        *)
-            shift
-            ;;
-    esac
-done
-
-# Function to export path based on platform
-export_path() {
-    if [ "$PLATFORM" = "mac" ]; then
-        # Get the path to the Python binary folder on Mac
-        PYTHON_BIN_PATH=$(dirname "$(which python3)")
-        export PATH="$PATH:$PYTHON_BIN_PATH"
-    elif [ "$PLATFORM" = "windows" ]; then
-        # Get the path to the Python binary folder on Windows
-        PYTHON_BIN_PATH=$(where python | head -n 1)
-        PYTHON_BIN_PATH=$(dirname "$PYTHON_BIN_PATH")
-        setx PATH "%PATH%;%PYTHON_BIN_PATH%"
-    fi
-}
-
-# Export the path only if COLAB is not set or set to false
-if [ -z "$COLAB" ] || [ "$COLAB" = false ]; then
-    export_path
-fi
-
+ 
+# Get the path to the Python binary folder
+PYTHON_BIN_PATH=$(dirname "$(which python3)")
+ 
+# Path where pip3 installs executables for the current user
+USER_PYTHON_BIN="/Users/devulapa/Library/Python/3.9/bin"
+ 
+export PATH="$PATH:$PYTHON_BIN_PATH:$USER_PYTHON_BIN"
+ 
 # Check if local-req.txt exists
 if [ -f "local-req.txt" ]; then
     # Install packages from local-req.txt
@@ -47,6 +16,6 @@ else
     echo "local-req.txt not found."
     exit 1
 fi
-
+ 
 git config --global user.name "interactive-dsa-book"
 git config --global user.email "kumar6017drive@gmail.com"
